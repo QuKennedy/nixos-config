4c49edc75bda5b7613fe03ce4192acfbefa43838
quinn <quinnkennedy9@gmail.com>
2026-04-02 21:01:00 -0400

    bt changes, probably no op though

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -2,4 +2,17 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
+
+  hardware.bluetooth.settings = {
+    Policy.AutoEnable = true;
+    General = {
+      FastConnectable = true;
+      ReconnectAttempts = 7;
+      ReconnectIntervals = "1, 2, 4, 8, 16, 32, 64";
+    };
+  };
+
+  boot.extraModprobeConfig = ''
+    options btusb enable_autosuspend=n
+  '';
 }
